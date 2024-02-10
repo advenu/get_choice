24f917a05f814c610ba6c55f5c5989a6b8e570ad
Crebuo <iamcrebuo@gmail.com>
2024-02-10 20:04:57 +0530

    update to version 0.0.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 DESCRIPTION = "CLI option chooser"
 LONG_DESCRIPTION = "Lets user choose from a list of options through cli."
 
